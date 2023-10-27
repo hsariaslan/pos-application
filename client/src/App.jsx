b0a13f142c4dbcfd7aef72f9f1530bf919351bc5
hsariaslan <hakan.sariaslan.job@gmail.com>
2023-10-27 11:22:23 +0300

    Create categories component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
 import Header from "./components/header/Header";
+import Categories from "./components/categories/Categories";
 
 const App = () => {
-  return (<div>
-    <Header/>
-  </div>);
+  return (
+    <div>
+      <Header/>
+      <div className="home flex justify-between px-6 gap-10">
+        <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_+134px)]">
+          <Categories />
+        </div>
+        <div className="products flex-[8]">
+          <div>products</div>
+        </div>
+        <div className="cart-totals">
+          <div>cart-totals</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
