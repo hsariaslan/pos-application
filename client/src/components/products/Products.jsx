950042c7db846a2b1465f0adcb6484ac90f3d0e9
hsariaslan <hakan.sariaslan.job@gmail.com>
2023-11-11 12:52:22 +0300

    Filter and search actions have been made working throughout the project

--- client/src/components/products/Products.jsx
+++ client/src/components/products/Products.jsx
@@ -1,34 +1,20 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {EditOutlined, PlusOutlined} from "@ant-design/icons";
 import ProductItem from "./ProductItem";
 import CreateProduct from "../products/CreateProduct";
 import "./products.css";
 import {useNavigate} from "react-router-dom";
 
-const Products = ({categories}) => {
-  const [products, setProducts] = useState([]);
+const Products = ({categories, products, setProducts, filteredProducts, search}) => {
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const getProducts = async () => {
-      try {
-        const res = await fetch(process.env.REACT_APP_API_URL + "/products/");
-        const data = await res.json();
-
-        setProducts(data);
-      } catch (e) {
-        console.log(e);
-      }
-    }
-
-    getProducts();
-  }, []);
-
   return (
     <div className="products-wrapper grid grid-cols-card gap-4">
-      {products.map((product) => (
-        <ProductItem product={product} key={product._id} />
+      {filteredProducts
+        .filter((product) => product.title.toLowerCase().includes(search))
+        .map((product) => (
+          <ProductItem product={product} key={product._id} />
       ))}
       <div className="product-item product-item-action bg-purple-800" onClick={() => setIsAddModalOpen(true)}>
         <PlusOutlined className="md:text-3xl text-white" />
